f56c324879e3daa5960a068fcf2b91484e3f555e
bons0002 <98049612+bons0002@users.noreply.github.com>
2024-11-07 17:38:03 -0600

    Fix neotest serial tests for Rust

--- lua/packages-configs/neotest-setup.lua
+++ lua/packages-configs/neotest-setup.lua
@@ -1,7 +1,7 @@
 require('neotest').setup({
     adapters = {
         require('neotest-rust') {
-            args = {'--run-ignored all'},
+            args = {'--run-ignored all', '--locked'},
         },
     },
 })
