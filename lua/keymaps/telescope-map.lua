c0e97bd421088fbc9b2d68e48e4b8ca74cb6748b
bons0002 <sam_bons@yahoo.com>
2025-12-02 13:37:46 -0600

    Update telescope git keymaps

--- lua/keymaps/telescope-map.lua
+++ lua/keymaps/telescope-map.lua
@@ -7,18 +7,22 @@ vim.cmd([[
     vnoremap <A-t> <ESC>:Telescope file_browser hidden=true no_ignore=true<CR>
 
     " Telescope git status
-    inoremap <A-g> <ESC>:Telescope git_status<CR>
-    nnoremap <A-g> :Telescope git_status<CR>
-    vnoremap <A-g> <ESC>:Telescope git_status<CR>
+    inoremap <A-g><A-s> <ESC>:Telescope git_status<CR>
+    nnoremap <A-g><A-s> :Telescope git_status<CR>
+    vnoremap <A-g><A-s> <ESC>:Telescope git_status<CR>
 
     " Telescope git commits
-    inoremap <A-y> <ESC>:Telescope git_commits<CR>
-    nnoremap <A-y> :Telescope git_commits<CR>
-    vnoremap <A-y> <ESC>:Telescope git_commits<CR>
+    inoremap <A-g><A-c> <ESC>:Telescope git_commits<CR>
+    nnoremap <A-g><A-c> :Telescope git_commits<CR>
+    vnoremap <A-g><A-c> <ESC>:Telescope git_commits<CR>
+
+    inoremap <A-g><A-b> <ESC>:Telescope git_bcommits<CR>
+    nnoremap <A-g><A-b> :Telescope git_bcommits<CR>
+    vnoremap <A-g><A-b> <ESC>:Telescope git_bcommits<CR>
 
     " Document symbols (functions, etc.)
-    inoremap <A-d> <ESC>:Telescope lsp_document_symbols<CR>
-    nnoremap <A-d> :Telescope lsp_document_symbols<CR>
-    vnoremap <A-d> <ESC>:Telescope lsp_document_symbols<CR>
+    inoremap <A-g><A-d> <ESC>:Telescope lsp_document_symbols<CR>
+    nnoremap <A-g><A-d> :Telescope lsp_document_symbols<CR>
+    vnoremap <A-g><A-d> <ESC>:Telescope lsp_document_symbols<CR>
 
 ]])
